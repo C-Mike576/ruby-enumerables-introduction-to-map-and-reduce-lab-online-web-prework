e0b85db67269812a261d15b34169bea2bc87a9bc
Michael Calkins <m.calkins576@gmail.com>
2019-10-22 19:39:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -57,8 +57,16 @@ def reduce_to_all_true(source_array)
   return true
 end
 
-def 
-
+def reduce_to_any_true(source_array)
+  i = 0
+  while i < source_array.length do
+    if source_array[i] == true
+      return true
+    end
+      i += 1
+    end
+  return false
+end
 
 
 
